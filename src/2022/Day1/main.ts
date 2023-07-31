00e4cdcf526cf556c10099cb36874e820c9f26e4
Danylo Tkach <danylo.tkach@sliceiq.com>
2023-07-31 23:02:20 +0300

    init

---
+++ src/2022/Day1/main.ts
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, 'data.txt'), 'utf8', (err: any, data: string) => {
+    if(err) {
+        console.error(err);
+        return;
+    }
+    
+    const elfInventories = data.trim().split("\n\n").map((inventory) => inventory.split("\n").map(Number));
+    const totalCaloriesPerElf = elfInventories.map((inventory) => inventory.reduce((sum, calories) => sum + calories, 0));
+
+    console.log('Part #1: ', Math.max(...totalCaloriesPerElf));
+
+    const topThree = totalCaloriesPerElf.sort((a, b) => b - a);
+    console.log('Part #2: ', topThree[0] + topThree[1] + topThree[2])
+})
