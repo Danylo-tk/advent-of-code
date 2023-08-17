5cdd11217945ea57047972d45bbd19c8b8b43661
Danylo Tkach <danylo.tkach@gmail.com>
2023-08-17 22:35:48 +0300

    Day3 Task #2 completion

--- src/2022/Day3/main.ts
+++ src/2022/Day3/main.ts
@@ -8,22 +8,18 @@ fs.readFile(path.join(__dirname, 'data.txt'), 'utf8', (err: any, data: string) =
   }
 
   console.log('Task #1: ', findCommonsSum(data));
-    
+  console.log('Task #2: ', findGroupsByThreeSum(data))    
 });
 
 const splitLine = (line: string) => [line.slice(0, line.length/2), line.slice(line.length/2)];
 
 const findCommon = (line: string[]) => {
   const sets = line.map((item) => new Set(item.split('')));
-  let common: string = '';
+  const common = [...sets[0]].filter((ch) => 
+    sets.every((set) => set.has(ch))
+  );
 
-  for(const el of sets[1]) {
-    if(sets[0].has(el)){
-      common = el;
-    }
-  }
-
-  return common;
+  return common[0];
 }
 
 const letterPriority = (letter: string) => {
@@ -41,4 +37,21 @@ const findCommonsSum = (data: string) => {
     .reduce((accum, currVal) => accum + currVal, 0);
 
   return commonCharsSum;
+}
+
+/// Task #2 ->
+
+const groupByThree = (inputArr: string[]) => {
+  const groups = [];
+  for(let i = 0; i < inputArr.length; i += 3) {
+    groups.push(inputArr.slice(i, i + 3));
+  }
+
+  return groups;
+}
+
+const findGroupsByThreeSum = (data: string) => {
+  const lines = groupByThree(data.split('\n'));
+
+  console.log(lines.map(findCommon).map(letterPriority).reduce((accum, currVal) => accum + currVal, 0));
 }
